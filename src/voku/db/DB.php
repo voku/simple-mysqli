62b736b79729d9116684b6fbca96c329396ef8a7
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-03-01 03:20:44 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/voku/db/DB.php
+++ src/voku/db/DB.php
@@ -352,7 +352,7 @@ Class DB
   /**
    * wrapper for a "Logger"-Class
    *
-   * @param array $log [method, text, type] e.g.: array('error', 'this is a error', 'sql')
+   * @param string[] $log [method, text, type] e.g.: array('error', 'this is a error', 'sql')
    */
   private function logger($log)
   {
@@ -825,8 +825,7 @@ Class DB
     } else if (($var instanceof \DateTime)) {
       try {
         $var = "'" . $this->escape($var->format('Y-m-d h:m:i'), false, false) . "'";
-      }
-      catch (\Exception $e) {
+      } catch (\Exception $e) {
         $var = null;
       }
     } else {

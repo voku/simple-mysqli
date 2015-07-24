f72fd5adfb53e370b9ed452077f381a7fb659759
Lars Moelleken <lmoelleken@menadwork.com>
2015-07-24 13:54:11 +0200

    [+]: use "psr-4" autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/src/voku/db/DB.php';
+require_once dirname(__DIR__) . '/src/voku/db/Result.php';
 
 /*
 CREATE DATABASE mysql_test;
